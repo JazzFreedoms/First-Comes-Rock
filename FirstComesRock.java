5faad5ffa2c05af9dfbe494344a5f7bb2ff20d08
JazzFreedoms <jazzfreedoms@gmail.com>
2015-02-27 09:54:04 -0500

    Removed extras

--- FirstComesRock.java
+++ FirstComesRock.java
@@ -10,29 +10,29 @@ public class FirstComesRock {
         Challenger star = new Challenger("star");
 
         System.out.println("Welcome to Geoffrey's First Comes Rock");
-        System.out.println("Would you like to play Standard Mode or Lizard/Spock Mode?");
+        //System.out.println("Would you like to play Standard Mode or Lizard/Spock Mode?");
         System.out.println("Type '1' for Standard Mode");
-        System.out.println("Type '2' for Lizard/Spock Mode");
+        //System.out.println("Type '2' for Lizard/Spock Mode");
         mode = s.nextInt();
         if (mode == 1) {
             System.out.println("You have chosen Standard Mode.");
             System.out.println("Which opponent do you want to challenge?");
             System.out.println("Type '1' to challenge Spongebob");
-            System.out.println("Type '2' to challenge Patrick");
+            //System.out.println("Type '2' to challenge Patrick");
             opponent = s.nextInt();
                 if (opponent == 1) {
                     System.out.println("You have chosen Spongebob.");
                     System.out.println("Spongebob: I'm ready!");
                     System.out.println("Type '0' for Rock, '1' for Paper, '2' for Scissors");
                     System.out.println(sponge.round(s.nextInt()));
-                } else if (opponent == 2) {
-                    System.out.println("You have chosen Patrick.");
-                    System.out.println("Patrick: Oh boy!");
-                    System.out.println("Type '0' for Rock, '1' for Paper, '2' for Scissors");
+                //} else if (opponent == 2) {
+                    //System.out.println("You have chosen Patrick.");
+                    //System.out.println("Patrick: Oh boy!");
+                    //System.out.println("Type '0' for Rock, '1' for Paper, '2' for Scissors");
                 } else {
                     System.out.println("Nigga you had ONE JOB"); 
                 }
-        } else if (mode == 2) {
+        //} else if (mode == 2) {
             
         } else {
             System.out.println("Nigga you had ONE JOB");
